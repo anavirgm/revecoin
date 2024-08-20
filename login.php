18da0842c58649e61b0a25b033940da4d0ab714d
Ana Mota <anavirgm@outlook.com>
2024-08-20 10:06:17 -0400

    update session

--- login.php
+++ login.php
@@ -1,3 +1,60 @@
+<?php
+session_start();
+include 'db_connection.php'; // archivo con la conexión a la base de datos
+
+$error_message = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtener el email y la contraseña del formulario
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Preparar la consulta SQL para obtener el usuario con el email proporcionado
+    $query = "SELECT id, nombres, contraseña, status FROM usuarios WHERE email = ?";
+    if ($stmt = $conn->prepare($query)) {
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->store_result();
+
+        // Verificar si se encontró un usuario con el email proporcionado
+        if ($stmt->num_rows == 1) {
+            $stmt->bind_result($id, $nombres, $hashed_password, $status);
+            $stmt->fetch();
+
+            // Verificar el estado del usuario
+            if ($status == 1) {
+                // Verificar la contraseña
+                if (password_verify($password, $hashed_password)) {
+                    // La contraseña es correcta, iniciar sesión
+                    $_SESSION['loggedin'] = true;
+                    $_SESSION['id'] = $id;
+                    $_SESSION['nombres'] = $nombres;
+                    header("Location: dashboard.php");
+                    exit();
+                } else {
+                    // Contraseña incorrecta
+                    $error_message = "La contraseña que has introducido no es correcta.";
+                }
+            } else {
+                // Usuario inactivo
+                $error_message = "Tu cuenta está inactiva. Contacta con el administrador.";
+            }
+        } else {
+            // Email no encontrado
+            $error_message = "No hay ninguna cuenta asociada con ese email.";
+        }
+
+        $stmt->close();
+    } else {
+        // Error en la preparación de la consulta
+        $error_message = "Error al preparar la consulta.";
+    }
+
+    $conn->close();
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -18,9 +75,18 @@
         <section id="login-section">
             <div class="login-container">
                 <h3>Inicio de Sesión</h3>
-                <p>------------------------------------</p>
+                <p>
+                    <hr class="custom-line">
+                </p>
 
-                <form action="login_processing.php" method="post">
+                <?php
+                // Mostrar mensaje de error (si hay uno)
+                if (!empty($error_message)) {
+                    echo "<p style='color:red; text-align:center;'>$error_message</p>";
+                }
+                ?>
+
+                <form action="login.php" method="post">
                     <div class="form-group">
                         <label for="email">E-mail</label>
                         <input type="email" id="email" name="email" required>
@@ -32,6 +98,8 @@
 
                     <button type="submit">Iniciar Sesión</button>
                 </form>
+
+                <p>¿Olvidó su contraseña? </p>
             </div>
         </section>
     </main>
@@ -66,7 +134,7 @@
             display: flex;
             justify-content: center;
             align-items: center;
-            height: calc(100vh - 60px); /* Ajusta la altura según sea necesario */
+            height: calc(90vh - 10px); /* Ajusta la altura según sea necesario */
             background-color: #f4f4f4;
         }
 
@@ -83,16 +151,40 @@
             font-size: 24px;
             font-weight: bold;
             color: #362465;
-            margin-bottom: 20px;
+            margin-top: 10px;
             text-align: center;
         }
 
         .login-container p {
-            font-size: 1rem;
-            color: #693DCF;
-            text-align: center;
-            margin-bottom: 20px;
-        }
+    font-size: 15px;
+    color:#565E65;
+    text-align: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    text-decoration: none;
+    position: relative;
+}
+
+.login-container p:hover {
+    text-decoration-color:#565E65;
+}
+
+.login-container p::before {
+    content: "";
+    position: absolute;
+    display: block;
+    width: 60%; /* Ajusta esto según sea necesario */
+    height: 2px;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%) scaleX(0);
+    background-color: #7179A8;
+    transition: transform 0.5s ease;
+}
+
+.login-container p:hover::before {
+    transform: translateX(-50%) scaleX(1);
+}
 
         .form-group {
             display: flex;
@@ -121,7 +213,7 @@
         .login-container button {
             font-family: 'Poppins', sans-serif;
             display: block;
-            margin: 40px auto;
+            margin: 40px auto 20px;
             padding: 10px 20px;
             background-color: #9568FF;
             color: white;
@@ -139,5 +231,14 @@
             transform: scale(1.05);
             box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.2);
         }
+
+        .custom-line {
+            border: 0; /* Eliminar el borde predeterminado */
+            height: 2px; /* Altura de la línea */
+            background-color: #7179A8; /* Color de la línea */
+            margin: 20px auto; /* Márgenes automáticos para centrar la línea */
+        }
+
+        
     </style>
 </html>
